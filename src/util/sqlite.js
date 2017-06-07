a4355166232d2620de4b250b67649a258e922433
Yaowenjie <wjyao@thoughtworks.com>
2017-06-07 11:22:10 +0800

    remove useless code

--- src/util/sqlite.js
+++ src/util/sqlite.js
@@ -7,11 +7,4 @@ const initDB = () => {
   db.prepare('CREATE TABLE IF NOT EXISTS ' + threadSchema).run();
 };
 
-// const fns = {
-//   db: db,
-//   initDB: () => {
-//     db.prepare('CREATE TABLE IF NOT EXISTS ' + threadSchema).run();
-//   }
-// };
-
 module.exports = {db, initDB};
